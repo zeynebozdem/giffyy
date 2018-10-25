86b635cafbace302059c8702f8f87590f1cfed69
zeynep özdem <zeynebozdem@gmail.com>
2018-10-25 18:58:36 +0300

    first

---
+++ categories.js
@@ -0,0 +1,9 @@
+var categories=["agree","applause","aww","dance","deal with it","do not want","eww","eye roll","face palm","good luck","good bye","hearts","high five","hug","idk","kiss","no","omg","ok","oops","please","popcorn","scared","seriusly","shocked","slow clap","sorry","thank you","thumbs down","thumbs up","want","win","you got this"];
+
+for(i=0;i<categories.length;i++){
+    var btn=document.createElement("BUTTON");
+    btn.className=categories[i].replace(/ /g,'');
+    var text=document.createTextNode(categories[i]);
+    btn.appendChild(text);
+   $('button').appendChild(btn);
+}
