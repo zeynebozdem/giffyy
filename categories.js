7585387a9977d68f8990e38bdf7e5e86f3cbcf91
zeynepozdem <zeynebozdem@gmail.com>
2018-10-25 23:14:34 +0300

    kategoriler eklendi

--- categories.js
+++ categories.js
@@ -3,7 +3,18 @@ var categories=["agree","applause","aww","dance","deal with it","do not want","e
 for(i=0;i<categories.length;i++){
     var btn=document.createElement("BUTTON");
     btn.className=categories[i].replace(/ /g,'');
+    btn.onclick=Results;
     var text=document.createTextNode(categories[i]);
     btn.appendChild(text);
-   $('button').appendChild(btn);
+    $('.categories').append(btn);
+   
 }
+
+function Results(){
+    alert(event.srcElement.className);
+}
+
+   /*$(`.${categories[i]}`).click(function(){
+    console.log(categories[i]);
+
+}); */
---
+++ random.js
@@ -0,0 +1,15 @@
+var apikey = 'QyoA3LBnlBLHUTh8cipaaJ56jXbXJoB2';
+
+$(".random").click(function(){
+
+    var xhr = $.get(`http://api.giphy.com/v1/gifs/random?&api_key=${apikey}&limit=1`);
+
+    xhr.done(function (response) {
+    console.log("random", response);
+    var array = response.data
+
+    $(".datadiv").append(`<img src=${array.images.original.url} style='height=350px;'/>`)
+
+});
+$(".datadiv").empty();
+});
