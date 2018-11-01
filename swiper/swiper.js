7ba8e6e41d990fbf2e86592a1af55f4f5cb75f0a
zeynepozdem <zeynebozdem@gmail.com>
2018-11-02 00:57:15 +0300

    responsive ui added

--- swiper/swiper.js
+++ swiper/swiper.js
@@ -4,6 +4,28 @@ var swiper = new Swiper('.swiper-container', {
     slidesPerView: 10,
     centeredSlides: false,
     spaceBetween: 10,
+    breakpoints: {  
+    320: {       
+        slidesPerView: 3,       
+        spaceBetween: 10     
+     },
+     480:{
+        slidesPerView: 4,       
+        spaceBetween: 10 
+     },
+     640:{
+        slidesPerView: 5,       
+        spaceBetween: 10 
+     },
+     768:{
+        slidesPerView: 6,       
+        spaceBetween: 10 
+     },
+     1024:{
+        slidesPerView: 7,       
+        spaceBetween: 10 
+     },
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -12,4 +34,5 @@ var swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    
 });
