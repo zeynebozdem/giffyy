58089ff18584a842c2a8e9b05b57245221578b71
zeynepozdem <zeynebozdem@gmail.com>
2018-11-12 01:23:49 +0300

    12.11.2018 / 01:23

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 var id; //1 trends, 2 search, 3 category
 var apikey = 'NinlyFZz6OQehrRKF5TafhvA59MxoI67';
 
-function GetData(skip,take,searchvalue){
-    var method=!searchvalue?"trending?":`search?q=${searchvalue}&`;
+function GetData(skip, take, searchvalue) {
+    var method = !searchvalue ? "trending?" : `search?q=${searchvalue}&`;
 
-var xhr = $.get(`http://api.giphy.com/v1/gifs/${method}api_key=${apikey}&limit=${take}&offset=${skip}`);
+    var xhr = $.get(`http://api.giphy.com/v1/gifs/${method}api_key=${apikey}&limit=${take}&offset=${skip}`);
     xhr.done(function (response) {
         console.log("categoryData", response);
         var results = response.data
 
         for (i in results) {
-            $(".data-gifs").append(` <div class="gif-container"><img src=${results[i].images.fixed_width.url}><div class="overlay"><div><a href="#" target="_blank"><img src="assets/clone.svg"></a></div><div><a href="#" target="_blank"><img src="assets/facebook.svg"></a></div><div><a href="#" target="_blank"><img src="assets/twitter.svg"></a></div><div><a href="# target="_blank"><img src="assets/whatsapp.svg"></a></div></div></div>`);
-             }
+            var gifLink = results[i].bitly_url;
+            $(".data-gifs").append(` <div class="gif-container"><img src=${results[i].images.fixed_width.url}><div class="overlay"><div onclick="copyClipboard('${results[i].bitly_url}')"><a><img src="assets/clone.svg"></a></div><div><a href="https://www.facebook.com/sharer.php?u=${results[i].bitly_url}" target="_blank"><img src="assets/facebook.svg"></a></div><div><a href="https://twitter.com/intent/tweet?url=${results[i].bitly_url}" target="_blank"><img src="assets/twitter.svg"></a></div></div></div>`);
+        }
 
     });
+}
+
+function copyClipboard(url) {
+    var copied = `${url}`;
+    copied.val().select();
+    console.log(copied);
+    document.execCommand("copy");
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copied);
 }
